21978ddada95116fdbb5c1238448e8bfefb9ff77
Johannes Valbjorn <johannes.valbjorn@gmail.com>
2017-08-10 09:05:16 +0200

    travis, fix dir_path for namedTempfiles to current directory

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,54 +3,35 @@ import os.path
 from cireqs.cli import cli
 from click.testing import CliRunner
 
+import os
+test_dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def test_expand_requirements():
-    with tempfile.NamedTemporaryFile(mode='w+t',
-                                     dir='/tmp') as requirements_to_expand_file, \
-            tempfile.NamedTemporaryFile(mode='w+t',
-                                        dir='/tmp') as requirements_file:
-        requirements_to_expand_file.writelines(['click-log==0.1.8'])
-        requirements_to_expand_file.seek(0)
 
-        dir_path, requirements_to_expand_filename = os.path.split(
-            requirements_to_expand_file.name)
+    input_requirements_filename = 'input_requirements_1.txt'
+    output_requirements_filename = 'output_requirements_1.txt'
 
-        requirements_filename = os.path.basename(requirements_file.name)
-        runner = CliRunner()
-        result = runner.invoke(cli,
-                               ['--dirpath', dir_path, 'expand_requirements',
-                                requirements_to_expand_filename,
-                                requirements_filename])
+    runner = CliRunner()
+    result = runner.invoke(cli,
+                           ['--dirpath', test_dir_path, 'expand_requirements',
+                            input_requirements_filename,
+                            output_requirements_filename])
 
-        requirements_file.seek(0)
 
-        asserted_output = ['click-log==0.1.8\n',
-                           '## The following requirements were added by pip freeze:\n',
-                           'click==6.7\n']
-        expanded_requirements_file_lines = requirements_file.readlines()
-        assert asserted_output == expanded_requirements_file_lines
-        assert result.exit_code == 0
+    asserted_output = ['click-log==0.1.8\n',
+                       '## The following requirements were added by pip freeze:\n',
+                       'click==6.7\n']
+    assert result.exit_code == 0
+    with open(os.path.join(test_dir_path, 'output_requirements_1.txt'), 'r') as output_requirements_file:
+        output_requirements_file_lines = output_requirements_file.readlines()
+        assert asserted_output == output_requirements_file_lines
 
 
 def test_verify_requirements():
-    with tempfile.NamedTemporaryFile(mode='w+t',
-                                     dir='/tmp') as requirements_file:
-        requirements_file.writelines(['click-log==0.1.8\n',
-                                      '## The following requirements were added by pip freeze:\n',
-                                      'click==6.7\n'])
-        requirements_file.seek(0)
-
-        dir_path, requirements_filename = os.path.split(requirements_file.name)
-        runner = CliRunner()
-        result = runner.invoke(cli,
-                               ['--dirpath', dir_path, 'verify_requirements',
-                                requirements_filename])
-
-        requirements_file.seek(0)
-
-        asserted_output = ['click-log==0.1.8\n',
-                           '## The following requirements were added by pip freeze:\n',
-                           'click==6.7\n']
-        expanded_requirements_file_lines = requirements_file.readlines()
-        assert asserted_output == expanded_requirements_file_lines
-        assert result.exit_code == 0
+    requirements_filename = 'input_requirements_2.txt'
+    runner = CliRunner()
+    result = runner.invoke(cli,
+                           ['--dirpath', test_dir_path, 'verify_requirements',
+                            requirements_filename])
+    assert result.exit_code == 0
