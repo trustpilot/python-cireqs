d76640fb871d49d8447ebd1bad4d028756a20c4f
sloev <johannes.valbjorn@gmail.com>
2018-12-14 11:54:45 +0100

    refactir test and add test for env var injection

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,15 +13,20 @@ def test_expand_requirements():
     output_requirements_filename = 'output_requirements_1.txt'
 
     runner = CliRunner()
+
     result = runner.invoke(cli,
-                           ['--dirpath', test_dir_path, 'expand',
+                           ['-v','DEBUG','--dirpath', test_dir_path, 'expand',
                             input_requirements_filename,
                             output_requirements_filename])
 
-    asserted_output = ['click-log==0.1.8\n',
-                       '## The following requirements were added by pip freeze:\n',
-                       'click==6.7\n']
-    print(result)
+    asserted_output = [
+        'cireqs==1.0.4\n',
+        '## The following requirements were added by pip freeze:\n',
+        'click==6.7\n',
+        'click-log==0.1.8\n'
+    ]
+
+    print(result.output)
     assert result.exit_code == 0
     with open(os.path.join(test_dir_path, 'output_requirements_1.txt'), 'r') as output_requirements_file:
         output_requirements_file_lines = output_requirements_file.readlines()
@@ -47,10 +52,27 @@ def test_timeout_error():
     assert result.exit_code == -1
 
 
-def test_verify_requirements():
+def test_verify_requirements_success():
     requirements_filename = 'input_requirements_3.txt'
     runner = CliRunner()
     result = runner.invoke(cli,
                            ['--dirpath', test_dir_path, 'verify',
                             requirements_filename])
+    print(result.output)
+    assert result.exit_code == 0
+
+def test_expand_requirements_with_env_var():
+    requirements_filename = 'input_requirements_3.txt'
+    runner = CliRunner()
+    result = runner.invoke(cli,
+                           ['--dirpath', test_dir_path, '-e' 'foo=bar', '--dry','verify',
+                            requirements_filename])
+    print(result.output)
+    docker_command_line = result.output
+    first_half = 'docker run --rm --name cireqs_container -e foo=bar -v '
+    last_half_index_begin = docker_command_line.index('-w /src')
+    docker_command_line_without_path = docker_command_line[:len(first_half)] + docker_command_line[last_half_index_begin:]
+
+    assserted_docker_command_line = "docker run --rm --name cireqs_container -e foo=bar -v -w /src python:3.6.1-alpine sh -c pip install --upgrade -q pip && pip install -q -r input_requirements_3.txt  && pip freeze -q -r input_requirements_3.txt"
+    assert assserted_docker_command_line == docker_command_line_without_path.strip()
     assert result.exit_code == 0
